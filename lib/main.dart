1188ff8a7eaeb2ff3beff71f30dfee51e087f2f5
qianyiii <B210230C@sc.edu.my>
2023-10-22 17:54:21 +0800

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:smart_checkout_system/welcome.dart';
 import 'package:smart_checkout_system/login.dart';
 import 'package:smart_checkout_system/registration.dart';
 import 'package:smart_checkout_system/home.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp( MyApp());
 }
 
